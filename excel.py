c32f6019bf153eb16119c55112f3315baa3d20fc
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2024-10-04 02:59:44 -0600

    Generate multiple sheets

--- excel.py
+++ excel.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+from openpyxl import Workbook
 
 
 def read_json_file(file_path):
@@ -18,58 +19,75 @@ def get_category_name(category_id):
     return ""
 
 
-# Create a dictionary to store image_id: [annotations]
-image_annotations = {}
-
 # Group annotations by image_id
+image_annotations = {}
 for annotation in json_data["annotations"]:
     image_id = annotation["image_id"]
     if image_id not in image_annotations:
         image_annotations[image_id] = []
     image_annotations[image_id].append(annotation)
 
-# Create a list to store the rows for the Excel file
-excel_rows = []
 
+def generate_data_frames(batch_size):
+    excel_rows = []
+    data_frames = []
 
-# Generate rows for the Excel file
-for image in json_data["images"]:
-    image_id = image["id"]
-    # Every PDF has 300 images.
-    page = (int(image_id) % 300) + 2
+    for image in json_data["images"]:
+        image_id = image["id"]
+        # Every PDF has batch_size images
+        page = (
+            int(image_id) % batch_size
+        ) + 2  # + 2 because the image_id starts at 0 and we need to ignore the legend page at the beginning
 
-    if image_id in image_annotations:
-        for i, annotation in enumerate(image_annotations[image_id]):
-            if i == 0:
-                excel_rows.append(
-                    [
-                        page,
-                        annotation["id"],
-                        get_category_name(annotation["category_id"]),
-                        "",
-                    ]
+        # Let's append what we have every batch and reset excel_rows
+        if page == 2 and len(excel_rows) > 0:
+            data_frames.append(
+                pd.DataFrame(
+                    excel_rows,
+                    columns=["Page", "Annotation Id", "Annotation Name", "Correct?"],
                 )
-            else:
-                excel_rows.append(
-                    [
-                        "",
-                        annotation["id"],
-                        get_category_name(annotation["category_id"]),
-                        "",
-                    ]
-                )
-    else:
-        excel_rows.append([page, "", "", ""])
+            )
+            excel_rows = []
+
+        if image_id in image_annotations:
+            for i, annotation in enumerate(image_annotations[image_id]):
+                if i == 0:
+                    excel_rows.append(
+                        [
+                            page,
+                            annotation["id"],
+                            get_category_name(annotation["category_id"]),
+                            "",
+                        ]
+                    )
+                else:
+                    excel_rows.append(
+                        [
+                            "",
+                            annotation["id"],
+                            get_category_name(annotation["category_id"]),
+                            "",
+                        ]
+                    )
+        else:
+            excel_rows.append([page, "-", "-", "-"])
+
+    # Append the rest of the images that didn't make it inside the for
+    data_frames.append(
+        pd.DataFrame(
+            excel_rows,
+            columns=["Page", "Annotation Id", "Annotation Name", "Correct?"],
+        )
+    )
+    return data_frames
 
 
-# Create a pandas DataFrame
-df = pd.DataFrame(
-    excel_rows,
-    columns=["Page", "Annotation Id", "Annotation Name", "Correct?"],
-)
+dfs = generate_data_frames(batch_size=300)
 
-# Export to Excel
-excel_filename = "image_annotations_output.xlsx"
-df.to_excel(excel_filename, index=False, engine="openpyxl")
+# Generate Excel with multiple sheets
+with pd.ExcelWriter("image_annotations_output.xlsx", engine="openpyxl") as writer:
+    for i, df in enumerate(dfs):
+        df.to_excel(writer, sheet_name=f"batch {i + 1}", index=False)
 
-print(f"Excel file '{excel_filename}' has been created successfully.")
+
+print(f"Excel file has been created successfully.")
